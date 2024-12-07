8920507f36fdd9b532e56034be3e2f18200dabae
Billy Eskein <billyeskein@gmail.com>
2024-12-07 07:14:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+const cards = ["Charlie", "Samir", "Ali"];
+const event = "surprise";
+
+function writeCards(cards, event) {
+    let messages = [];
+
+    for(let i = 0; i < cards.length; i++) {
+        let message = `Thank you, ${cards[i]}, for the wonderful ${event} gift!`;
+messages.push(message);
+    }
+    return messages;
+}
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
